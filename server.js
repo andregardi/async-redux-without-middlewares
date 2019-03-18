c67d60824cd0d1121daf1f21b996acaeba8b9d05
Koury <andre.koury@accenture.com>
2019-03-18 12:16:25 -0300

    express server to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
